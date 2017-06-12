6e709de314fec4c7a876e9e4fbf8e8a716077875
Siddarth07 <siddarthkethireddy@gmail.com>
2017-06-12 11:13:29 -0400

    first save

---
+++ TypingSpeed/TimerImplementation.swift
@@ -0,0 +1,16 @@
+//
+//  TimerImplementation.swift
+//  TypingSpeed
+//
+//  Created by Siddarth Kethireddy on 6/12/17.
+//  Copyright © 2017 Siddarth Kethireddy. All rights reserved.
+//
+
+import Foundation
+
+class timerImplementation{
+    func beginCount(){
+        
+    }
+    
+}
--- TypingSpeed/ViewController.swift
+++ TypingSpeed/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+  
+    //Function Call to Timer
+    var numButtonPress=0;
+    @IBOutlet weak var countdownText: UILabel!
+    var numSeconds = 60;
+    var timer = Timer()
+    @IBOutlet weak var userTextField: UITextField!
 
+
+    
+    @IBAction func pressInUserTextField(_ sender: Any) {
+        if(numButtonPress==0){
+            numButtonPress += 1
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.implementCounter), userInfo: nil, repeats: true)
+        }
+        
+    }
+    
+  
+    func implementCounter(){
+        numSeconds -= 1
+        countdownText.text = String(numSeconds)
+        if(numSeconds==0){
+            timer.invalidate()
+        }
+    }
+    
+    
+    @IBAction func resetButton(_ sender: Any) {
+        numSeconds = 60
+        countdownText.text = "60"
+        timer.invalidate()
+        
+    }
 
 }
 
