f477f2291f176d8f4b92aeeec3679f9d31601db3
Siddarth07 <siddarthkethireddy@gmail.com>
2017-06-12 09:41:23 -0400

    Initial Commit

---
+++ TypingSpeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TypingSpeed
+//
+//  Created by Siddarth Kethireddy on 6/12/17.
+//  Copyright © 2017 Siddarth Kethireddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
